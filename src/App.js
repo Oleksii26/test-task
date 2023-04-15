cad059f61962c0de996a72a412bfa8025a0af3fd
Oleksii26 <Kolesnik26@ukr.net>
2023-04-15 15:37:25 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fucken Russian pigs must die
+          Pidarasy
         </a>
       </header>
     </div>
